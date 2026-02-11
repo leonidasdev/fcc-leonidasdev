039e00fdb30bed26db404600e4e02e8b1238cdee
leonidasdev <leochenjin@gmail.com>
2026-02-11 17:15:59 +0100

    feat: quality assurance advancednode boilerplate

---
+++ quality_assurance/boilerplate-advancednode/server.js
@@ -0,0 +1,27 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const pug = require('pug');
+const app = express();
+
+app.set('view engine','pug');
+app.set('views','./views/pug');
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.route('/').get((req, res) => {
+  const cors = require('cors');
+  app.use(cors());
+  res.render('index');
+
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
